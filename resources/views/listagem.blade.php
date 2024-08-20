b099c3b516d789889b475b4073e417948e6bba56
Carlos Augusto <c.augustomagnus@gmail.com>
2024-08-20 18:05:33 -0300

    producao

--- resources/views/listagem.blade.php
+++ resources/views/listagem.blade.php
@@ -1,17 +1,14 @@
-<html>
-    <head>
-        <title>Controle de estoque</title>
-        <link href="/css/app.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+@extends('principal')
 
-    </head>
-    <body>
-        <div class="container">
+@section('conteudo')
+    @if(empty($produtos))
+        <div class="alert alert-danger">Você não tem nenhum produto cadastrado.</div>
+    @else
+
             <h1>Listagem de produtos</h1>
             <table class="table table-striped table-bordered table-hover">
                 @foreach($produtos as $produto)
-                <tr>
+                <tr class="danger">
                         <td>{{$produto->nome}}</td>
                         <td>{{$produto->valor}}</td>
                         <td>{{$produto->descricao}}</td>
@@ -21,9 +18,8 @@
                                 <span class="fas fa-search"></span>
                             </a>
                         </td>
-                    </tr>
+                </tr>
                 @endforeach
             </table>
-        </div>
-    </body>
-</html>
+    @endif
+@stop
